041300f1c3469658008147026e5702bf2c1228cc
Charlie Hanekamp <charlie@techorse.org>
2023-02-10 20:49:42 +0100

    Change class naming for the rating card

--- src/components/ratingCard/RatingCard.tsx
+++ src/components/ratingCard/RatingCard.tsx
@@ -10,23 +10,23 @@ export function RatingCard({ handleSubmit }: { handleSubmit: Function }) {
 
   return <Card children={
     <>
-      <div className='card__img'><img src="/icon-star.svg" alt="" /></div>
-      <h1 className='card__title'>How did we do?</h1>
-      <p className='card__description'>
+      <div className='rating_card__img'><img src="/icon-star.svg" alt="" /></div>
+      <h1 className='rating_card__title'>How did we do?</h1>
+      <p className='rating_card__description'>
         Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
       </p>
-      <div className='card__ratings'>
+      <div className='rating_card__ratings'>
         {[1, 2, 3, 4, 5].map((rate: number) => (
           <button
             type="button"
             onClick={() => handleRating(rate)}
-            className={`card__rating ${rating == rate ? "active" : ""}`}
+            className={`rating_card__rating ${rating == rate ? "active" : ""}`}
             key={rate}>
             <p>{rate}</p>
           </button>
         ))}
       </div>
-      <button onClick={() => handleSubmit(rating)} className='card__button'>Submit</button>
+      <button onClick={() => handleSubmit(rating)} className='rating_card__button'>Submit</button>
     </>
   } />
 }
