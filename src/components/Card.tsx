c94d22a62ce290cbfa935a505f341a28963fdb67
Charlie Hanekamp <charlie@techorse.org>
2023-02-10 15:16:02 +0100

    Style card ratings

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,12 +7,12 @@ export function Card() {
     <p className='card__description'>
       Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
     </p>
-    <div className='card__rating'>
-      <p className='card__rating'>1</p>
-      <p className='card__rating'>2</p>
-      <p className='card__rating'>3</p>
-      <p className='card__rating'>4</p>
-      <p className='card__rating'>5</p>
+    <div className='card__ratings'>
+      {[1, 2, 3, 4, 5].map((rate: number) => (
+        <div className='card__rating' key={rate}>
+          <p>{rate}</p>
+        </div>
+      ))}
     </div>
     <button className='card__button'>Submit</button>
   </div>
