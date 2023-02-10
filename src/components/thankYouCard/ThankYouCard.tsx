f2650bd189d60144fe0b0f375bb31aeb38a629c2
Charlie Hanekamp <charlie@techorse.org>
2023-02-10 20:58:13 +0100

    Add thank you image

--- src/components/thankYouCard/ThankYouCard.tsx
+++ src/components/thankYouCard/ThankYouCard.tsx
@@ -5,7 +5,7 @@ import './thankYouCard.css'
 export function ThankYouCard() {
   return <Card children={
     <>
-      Thank you
+      <div className='thank_you_card__img'><img src="/illustration-thank-you.svg" alt="" /></div>
     </>
   } />
 }
